4c8d20bedb0b419c5fadf03050bef294e2e10ec0
Rae Nadya <rae.nadya511@gmail.com>
2023-06-28 03:42:48 +0800

    Initial Commit

---
+++ Useless but Amusing/Useless_but_AmusingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Useless_but_AmusingApp.swift
+//  Useless but Amusing
+//
+//  Created by Kierae NJ on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Useless_but_AmusingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
